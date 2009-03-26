229a3f3eed39f73326968b03138eaf7e7595136d
Colin Campbell-McPherson <colin@logaan.net>
2009-03-26 15:57:18 +0900

    now spinning up a process from handshake

--- bittorrent.erl
+++ bittorrent.erl
@@ -1,19 +1,34 @@
 -module(bittorrent).
--export([handshake/2]).
+-export([start_peer/2]).
+-compile(export_all).
 
-handshake(Host, Port) ->
+start_peer(Host, Port) ->
   {ok, Socket} = gen_tcp:connect(Host, Port, [binary, {packet, 0}]),
-  ok = gen_tcp:send(Socket, <<19,
-                              "BitTorrent protocol",
-                              0,0,0,0,0,0,0,0,
-                              55,17,201,153,197,215,145,33,109,167,182,47,182,199,5,250,166,103,45,134,
-                              "-AZ4004-znmphhbrij37">>),
-  receive_handshake(Socket).
-
-receive_handshake(Socket) ->
+  ok = gen_tcp:send(Socket, list_to_binary([
+    19, % Protocol string length
+    "BitTorrent protocol", % Protocol string
+    <<0,0,0,0,0,0,0,0>>, % Reseved space
+    <<60, 12, 28, 181, 105, 136, 94, 238, 188, 226, 221, 253, 177, 52, 40, 78, 111, 80, 4, 172>>, % Info Hash
+    "-AZ4004-znmphhbrij37" % Peer ID
+  ])),
   receive
     {tcp,Socket,Bin} ->
-      <<PstrLen:1/binary, Pstr:19/binary, Reserved:8/binary, InfoHash:20/binary, PeerID:20/binary>> = Bin,
-      {PstrLen, Pstr, Reserved, InfoHash, PeerID}
+      <<
+        _PstrLen:1/binary,
+        _Pstr:19/binary,
+        _Reserved:8/binary,
+        InfoHash:20/binary,
+        _PeerID:20/binary
+      >> = Bin,
+      spawn(fun() -> peer_loop(Socket, InfoHash) end)
+  end.
+
+peer_loop(Socket, InfoHash) ->
+  receive
+    i_love_you ->
+      io:format("keep it coming baby\n"),
+      peer_loop(Socket, InfoHash);
+    kill_yourself ->
+      io:format("i hate you so much\n")
   end.
 
