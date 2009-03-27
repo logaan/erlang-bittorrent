3191b6611fa87e3247403d616b04863f83833c00
Colin Campbell-McPherson <colin@logaan.net>
2009-03-28 02:03:55 +0900

    moved info hash to a parameter added recognition of bitfield re-arranged a little removed get socket

--- bittorrent.erl
+++ bittorrent.erl
@@ -1,28 +1,15 @@
 -module(bittorrent).
--export([start_peer/2]).
+-export([start_peer/3]).
 -compile(export_all).
 
-start_peer(Host, Port) ->
-  spawn(?MODULE, start_loop, [Host, Port]).
+start_peer(Host, Port, InfoHash) ->
+  spawn(?MODULE, start_loop, [Host, Port, InfoHash]).
 
-start_loop(Host, Port) ->
+start_loop(Host, Port, InfoHash) ->
   io:format("Starting loop~n"),
   {ok, Socket} = gen_tcp:connect(Host, Port, [binary, {active, true}]),
-  % Manhattan Murder Mystery (Woody Allen)
-  ok = send_handshake(Socket, <<200, 23, 161, 167, 183, 43, 142, 38, 51, 130, 26, 222, 105, 118, 208, 38, 170, 97, 237, 59>>),
+  ok = send_handshake(Socket, InfoHash),
   loop(Socket).
-
-send_handshake(Socket, InfoHash) ->
-  gen_tcp:send(Socket, list_to_binary([
-      19,                    % Protocol string length
-      "BitTorrent protocol", % Protocol string
-      <<0,0,0,0,0,0,0,0>>,   % Reseved space
-      InfoHash,              % Info Hash
-      "-AZ4004-znmphhbrij37" % Peer ID
-    ])).
-
-send_keepalive(Socket) ->
-  gen_tcp:send(Socket, <<0,0,0,0,0,0,0,0>>).
 
 loop(Socket) ->
   receive
@@ -35,27 +22,42 @@ loop(Socket) ->
       >>} ->
       io:format("Received handshake from ~p~n", [PeerID]),
       bittorrent:loop(Socket);
+
+    {tcp,Socket,<<_MessageLength:4/binary, 5, _Tail/binary>>} ->
+      io:format("Received bitfield~n"),
+      bittorrent:loop(Socket);
+
     {tcp,Socket,<<0,0,0,0,0,0,0,0>>} ->
       io:format("Got a keepalive~n"),
       send_keepalive(Socket),
       bittorrent:loop(Socket);
+
     {tcp,Socket,<<0,0,0,1,1>>} ->
       io:format("You are unchoked~n"),
       bittorrent:loop(Socket);
+
     {tcp_closed,Socket} ->
       io:format("Connection was closed~n");
+
     {ReturnAddress, socket} ->
       ReturnAddress ! {ok, Socket},
       bittorrent:loop(Socket);
+
     Anything ->
       io:format("Got ~p~n", [Anything]),
       bittorrent:loop(Socket)
+
   end.
 
-receive_socket(Pid) ->
-  Pid ! {self(), socket},
-  receive
-    {ok, Socket} ->
-      Socket
-  end.
+send_handshake(Socket, InfoHash) ->
+  gen_tcp:send(Socket, list_to_binary([
+      19,                    % Protocol string length
+      "BitTorrent protocol", % Protocol string
+      <<0,0,0,0,0,0,0,0>>,   % Reseved space
+      InfoHash,              % Info Hash
+      "-AZ4004-znmphhbrij37" % Peer ID
+    ])).
+
+send_keepalive(Socket) ->
+  gen_tcp:send(Socket, <<0,0,0,0,0,0,0,0>>).
 
--- test.erl
+++ test.erl
@@ -2,6 +2,7 @@
 
 main(_) ->
   make:files([bittorrent]),
-  Pid = bittorrent:start_peer('localhost', 51413),
+  % Manhattan Murder Mystery (Woody Allen)
+  Pid = bittorrent:start_peer('localhost', 51413, <<200, 23, 161, 167, 183, 43, 142, 38, 51, 130, 26, 222, 105, 118, 208, 38, 170, 97, 237, 59>>),
   timer:sleep(infinity).
 
