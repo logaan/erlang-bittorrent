697967fd8eee5817e9105c2068b038aa554228aa
Colin Campbell-McPherson <colin@logaan.net>
2009-03-28 01:51:41 +0900

    added match for handshake response

--- bittorrent.erl
+++ bittorrent.erl
@@ -6,6 +6,7 @@ start_peer(Host, Port) ->
   spawn(?MODULE, start_loop, [Host, Port]).
 
 start_loop(Host, Port) ->
+  io:format("Starting loop~n"),
   {ok, Socket} = gen_tcp:connect(Host, Port, [binary, {active, true}]),
   % Manhattan Murder Mystery (Woody Allen)
   ok = send_handshake(Socket, <<200, 23, 161, 167, 183, 43, 142, 38, 51, 130, 26, 222, 105, 118, 208, 38, 170, 97, 237, 59>>),
@@ -25,6 +26,15 @@ send_keepalive(Socket) ->
 
 loop(Socket) ->
   receive
+    {tcp,Socket,<<
+        19,
+        "BitTorrent protocol",
+        _Reserved:8/binary,
+        _InfoHash:20/binary,
+        PeerID:20/binary
+      >>} ->
+      io:format("Received handshake from ~p~n", [PeerID]),
+      bittorrent:loop(Socket);
     {tcp,Socket,<<0,0,0,0,0,0,0,0>>} ->
       io:format("Got a keepalive~n"),
       send_keepalive(Socket),
