8470206343d7cb7404cf5bd08cf4464d0d9bb053
Colin Campbell-McPherson <colin@logaan.net>
2011-07-05 05:33:45 +1000

    removed some cruft. communicating back send haves. cleaner availables.

--- experements/need_server.erl
+++ experements/need_server.erl
@@ -1,31 +1,25 @@
 #!/usr/bin/env escript
 
 main(_) ->
-  Pid = spawn_link(fun() -> need_server(10) end),
+  Controller = self(),
+  Pid = spawn_link(fun() -> need_server(Controller, 10) end),
   process_flag(trap_exit, true),
   Pid ! {set_status, 7, received},
   timer:sleep(2000),
   Pid ! done,
-  receive Anything -> erlang:display(Anything) end.
+  regurgitate().
 
-need_server(NumberOfPieces) ->
-  % create an set type ets
+need_server(Controller, NumberOfPieces) ->
   Needs = ets:new(needs, []),
   populate(Needs, NumberOfPieces),
-  ets:update_element(Needs, 2, {2, received}),
+  loop(Controller, Needs).
 
-  erlang:display(ets:member(Needs, 1)),
-  erlang:display(ets:member(Needs, 3)),
-  erlang:display(ets:match(Needs, {'$1', available})),
-  erlang:display(ets:match(Needs, {'$1', received})),
-  loop(Needs).
-
-loop(Needs) ->
+loop(Controller, Needs) ->
   receive
     {set_status, PieceIndex, Status} ->
       io:format("Piece ~p ~p~n", [PieceIndex, Status]),
       ets:update_element(Needs, PieceIndex, {2, Status}),
-      loop(Needs);
+      loop(Controller, Needs);
     done ->
       exit(normal);
     Unknown ->
@@ -33,10 +27,11 @@ loop(Needs) ->
   after 1000 ->
       case length(ets:match(Needs, {'_', requested})) of
         0 ->
-          {Availables, _} = ets:match(Needs, {'$1', available}, 5),
-          io:format("Send out some haves for ~p~n", [Availables]);
+          {Availables, _Continuation} = ets:match(Needs, {'$1', available}, 5),
+          FormattedAvailables = [ A || [A] <- Availables ],
+          Controller ! {send_haves, FormattedAvailables};
         _ ->
-          loop(Needs)
+          loop(Controller, Needs)
       end
   end.
 
@@ -45,3 +40,12 @@ populate(Needs, 0) ->
 populate(Needs, NumberOfPieces) ->
   ets:insert(Needs, {NumberOfPieces, available}),
   populate(Needs, NumberOfPieces - 1).
+
+regurgitate() ->
+  receive
+    Anything ->
+      erlang:display(Anything),
+      regurgitate()
+  after 5000 ->
+      ok
+  end.
